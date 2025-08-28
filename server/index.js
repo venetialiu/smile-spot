d97adeda745f30b105820f7912014174bad75c74
Venetia Liu <venetia@venetia-2.mynetworksettings.com>
2025-08-27 22:39:32 -0400

    deployment updates

--- server/index.js
+++ server/index.js
@@ -17,8 +17,13 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Hello to smile spot API');
+})
+
 // database
 const PORT = process.env.PORT || 4000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
